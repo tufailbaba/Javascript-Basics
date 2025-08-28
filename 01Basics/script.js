4fbd8599146dacc11e47a57f5f2d6c2cbf2f65df
tufailbaba <tufailbabacoc@gmail.com>
2025-08-28 11:24:19 +0530

    learning

---
+++ 01Basics/script.js
@@ -0,0 +1,14 @@
+const accountId =112233
+let accountEmail = "tufail@example.com";
+var accountPassword = "12345";
+accountCity ="Sopore"
+
+// accountId = 2// not allowed
+accountEmail = "newemail@example.com";
+accountPassword = "67890";
+accountCity = "New City";
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
