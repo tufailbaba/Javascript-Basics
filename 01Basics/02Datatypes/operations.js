2b4ad11a1341d8c5a372008fa45b5b61447e5b30
Tufail <tufailbabacoc@gmail.com>
2025-09-03 15:05:30 +0530

    Practicing

--- 01Basics/02Datatypes/operations.js
+++ 01Basics/02Datatypes/operations.js
@@ -1,3 +1,18 @@
-let Value = 3;
-let negvalue = -Value;
+let Value = 4;
+ negvalue = -Value;
 console.log(negvalue);
+
+let str1 ="Hello";
+let str2 =" World";
+let str3= str1 + str2;  
+
+console.log(str1 + str2); 
+
+let number1 = 3;
+let number2 = 4;
+let sum = number1 + number2;
+console.log("Sum:", sum); 
+
+let  gamecounter = 100;
+gamecounter++;
+console.log("Game Counter:", gamecounter);
