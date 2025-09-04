027b3739fa675b09306077ed43eda803b129fd61
Tufail <tufailbabacoc@gmail.com>
2025-09-04 12:15:31 +0530

    Strings

---
+++ 01Basics/02Datatypes/string.js
@@ -0,0 +1,15 @@
+const fname = "Tufail";
+
+const lname = "Baba";
+
+const marks = 100;
+
+// console.log(fname + lname + marks + "Tuffi");
+
+console.log(
+  `Hello my First is ${fname} and my last name is ${lname} and i got ${marks} marks in exams`
+);
+
+const gameName = new String('Tufail');
+
+console.log(gameName.charAt(0));
