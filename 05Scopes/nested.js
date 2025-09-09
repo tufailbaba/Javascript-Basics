958a051047a8e03898a060fe7cc63e1c8cb98b31
Tufail <tufailbabacoc@gmail.com>
2025-09-09 13:53:19 +0530

    Nested

---
+++ 05Scopes/nested.js
@@ -0,0 +1,44 @@
+function one(){
+    const username ="Tufail";
+    function two(){
+        const website ="example.com";
+        console.log(username);
+
+        
+    }
+    //console.log(website);
+    two();
+
+    
+}
+one();
+
+if(true){
+
+    const username ="Tufail";   
+    if(username === "Tufail"){
+    const website = "youtube"
+    console.log(username +website);
+    
+    
+    }
+   //console.log(website);
+
+   
+}
+
+//console.log(username);
+
+
+function addone(num){
+   return num + 1;
+}
+addone(4);
+
+const addTWo = function(num){
+    return num + 2;
+}
+
+addTWo(4);
+console.log(addone(4));
+console.log(addTWo(4));
