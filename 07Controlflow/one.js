5b3d99313fd37541e46b4a335765c7102c8e2a05
Tufail <tufailbabacoc@gmail.com>
2025-09-11 11:22:11 +0530

    If & If Else

---
+++ 07Controlflow/one.js
@@ -0,0 +1,25 @@
+const isuserLoggedIn = !true;
+if(isuserLoggedIn){
+    console.log("Hello, World!");
+
+}
+if(!isuserLoggedIn){
+    console.log("Hello, boka");
+
+}
+
+if(3 == 3){
+    console.log("khandu");
+
+
+}
+else{
+    console.log("don");
+}
+const temp =40
+if(temp < 40){
+    console.log("its is hot day");
+}
+else if(temp == 40){
+    console.log("its is  a good day");
+}   
