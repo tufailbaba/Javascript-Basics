e19e1e45c3d49bfc41cef67c7412e7112be1f9c9
Tufail <tufailbabacoc@gmail.com>
2025-09-12 07:53:31 +0530

    Switch

--- 07Controlflow/one.js
+++ 07Controlflow/one.js
@@ -1,25 +1,49 @@
 const isuserLoggedIn = !true;
-if(isuserLoggedIn){
-    console.log("Hello, World!");
-
+if (isuserLoggedIn) {
+  console.log("Hello, World!");
 }
-if(!isuserLoggedIn){
-    console.log("Hello, boka");
-
+if (!isuserLoggedIn) {
+  console.log("Hello, boka");
 }
 
-if(3 == 3){
-    console.log("khandu");
+if (3 == 3) {
+  console.log("khandu");
+} else {
+  console.log("don");
+}
+const temp = 40;
+if (temp < 40) {
+  console.log("its is hot day");
+} else if (temp == 40) {
+  console.log("its is  a good day");
+}
+const balance = 1000;
+// if (balance > 500) console.log("you have more than 1000rs");
+// else console.log("you have less than 1000rs");
+if (balance < 500) {
+    console.log("less than");
+}
+else if (balance < 750) {
+    console.log("less than 750");
+}
+else if (balance < 900) {
+    console.log("less than 900");
+}
+else {
+    console.log(" your balance is  1000rs");
+}
 
-
+const userLoggedIn = true;
+const debitCard = true;
+if (userLoggedIn && debitCard) {
+    console.log("Allow to buy Courses");
 }
 else{
-    console.log("don");
+    console.log("chala ja bsdk");
 }
-const temp =40
-if(temp < 40){
-    console.log("its is hot day");
+const LoggedInFromgoogle = true;
+const LoggedInFromemail = true;
+
+if (LoggedInFromemail || LoggedInFromgoogle) {
+    console.log("User logged in");
 }
-else if(temp == 40){
-    console.log("its is  a good day");
-}   
